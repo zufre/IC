a4e9d484d5854c49364a7a90c7656f2cf9147b9a
Cédric David <cedric.david1974@gmail.com>
2019-09-03 17:42:34 +1000

    Create CalculatorModel.cs

---
+++ mvcDemo/Models/CalculatorModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcDemo030919.Models
+{
+    public class CalculatorModel
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Result { get; set; }
+    }
+}
