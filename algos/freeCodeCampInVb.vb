33d4516766ed55334538010e07bc66f8d7a6a372
Cédric David <cedric.david1974@gmail.com>
2019-11-05 23:44:00 +1000

    Update freeCodeCampInVb.vb

--- algos/freeCodeCampInVb.vb
+++ algos/freeCodeCampInVb.vb
@@ -2,22 +2,34 @@ Imports System
 				
 Public Module Module1
 	Public Sub Main()
-		Dim intArray As Integer(,) = New Integer(3, 3) {
-        {1, 2, 3, 5},
-        {1, 33, 3, 4},
-        {1, 55, 3, 4},
-        {22, 2, 3, 4}}
-		For Each item AS Integer In larg(intArray) 
-		Console.WriteLine(item)
-	Next
+		REM Dim intArray As Integer(,) = New Integer(3, 3) {
+        REM {1, 2, 3, 5},
+       REM  {1, 33, 3, 4},
+        REM {1, 55, 3, 4},
+        REM {22, 2, 3, 4}}
+		REM For Each item AS Integer In larg(intArray) 
+		REM Console.WriteLine(item)
+	REM Next
+	Console.WriteLine(confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "nothing"))
 	End Sub
+Public Function confirmEnding(ByVal str As String, target AS String) AS Boolean
+	Dim lengthStr As Integer = str.Length
+	Dim lengthTarget As Integer = target.Length
+	
+	If str.Substring(lengthStr-lengthTarget,lengthTarget)=target
+		Return true
+	Else 
+		Return false
+	End If
+	
+	End Function
 	 Public Function larg(ByVal arr As Integer(,)) As Integer()
         Dim newArr As Integer() = New Integer(3) {}
 		
-        For i As Integer = 0 To 4 - 1
+        For i As Integer = 0 To 3
             Dim largestNum As Integer = 0
 
-            For a As Integer = 0 To 4 - 1
+            For a As Integer = 0 To 3
 
                 If arr(i, a) > largestNum Then
                     largestNum = arr(i, a)
